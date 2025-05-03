4155893a4f485b7e1f806ef72dee64183abe324b
Pmoe97 <pchristiansen1019@gmail.com>
2025-05-02 23:12:33 -0400

    Conversations

--- dev/js/03_macros/dialogue-phase-triggers/bert.js
+++ dev/js/03_macros/dialogue-phase-triggers/bert.js
@@ -3,7 +3,7 @@ setup.bert_Conversation_Options_Phase0 = function () {
 		["I want to get to know you better.", "Bert_DenyMinigame", 0],
 		["What is on the menu?", "Bert_OpenShop", 1],
 		["What kind of drink would you make for me?", "Bert_CustomDrink", 0],
-		["You seem really serious.", "Bert_SeriousResponse", 0],
-		["Nevermind.", "ReturnToParlor", 1]
+		["Why so serious?", "Bert_SeriousResponse", 0],
+		["Nevermind.", "Bert_ReturnToParlor", 1]
 	]);
 };
