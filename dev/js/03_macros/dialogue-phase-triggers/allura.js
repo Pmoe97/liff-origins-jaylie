77b973727f8cb7e8429f2aa7e8d3aeca55f9cad6
Pmoe97 <Pchristiansen1019@gmail.com>
2025-05-08 16:52:19 -0400

    #convobox pre-change

--- dev/js/03_macros/dialogue-phase-triggers/allura.js
+++ dev/js/03_macros/dialogue-phase-triggers/allura.js
@@ -10,3 +10,25 @@ setup.allura_Conversation_Options_Phase0 = function () {
 		}
 	]);
 };
+
+setup.allura_Conversation_Options_Phase1 = function () {
+	setup.smartChoices([
+		{ label: "Your room is beautiful. Do they all look like this?", target: "YourRoom" },
+		{ label: "So… what do we do now?", target: "WhatNow" },
+		{ label: "Do you mind if I get comfortable with you?", target: "GetComfortable" },
+		{ label: "Would it be alright if I asked about you?", target: "BackstoryStart" },
+		{ label: "Can I kiss you?", target: "Kiss" },
+		{ label: "If it's alright… I’d like to just lay here with you.", target: "JustLay", reuse: 1 },
+		{ label: "We can talk more later.", target: "ReturnToParlor", reuse: 1 }
+	]);
+};
+
+setup.allura_Conversation_Options_Backstory = function () {
+	setup.smartChoices([
+		{ label: "Where did you grow up?", target: "WhereGrewUp" },
+		{ label: "What was your childhood like?", target: "Childhood" },
+		{ label: "Do you know who your parents were?", target: "Parentage" },
+		{ label: "If you could go anywhere in the world, where would it be?", target: "TravelWish" },
+		{ label: "Let’s talk about something else.", target: "BackstoryExit", reuse: 1 }
+	]);
+};
